b4bd63fe226f1e953a855fff5d5fd3371da11147
AppDevIn <teamprojectlive@gmail.com>
2020-08-25 02:32:36 +0800

    Created a widgets folder and added drawer in there

--- restaurantsapp/lib/main_meun.dart
+++ restaurantsapp/lib/main_meun.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:restaurantsapp/check_out.dart';
-
-import 'profile.dart';
-import 'feedback_form.dart';
-import 'user_info.dart';
-
+import 'widgets/drawer.dart';
 
 class MainMeun extends StatefulWidget {
   @override
@@ -18,76 +13,7 @@ class _MainMeunState extends State<MainMeun> {
       appBar: AppBar(
         title: Text('Main Meun'),
       ),
-      drawer: new Drawer(
-        child: ListView(
-          children: <Widget>[
-            new UserAccountsDrawerHeader(
-                accountName: new Text('Sam'),
-                accountEmail: new Text('jagateesvaran'),
-              currentAccountPicture: new CircleAvatar(
-                backgroundImage: new AssetImage('assets/images/profile_picture.jpg'),
-              ),
-            ),
-            new ListTile(
-              title: new Text('Profile Info'),
-              onTap: () {
-                Navigator.of(context).pop();
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => UserInfo()),
-                );
-              },
-            ),
-
-            new Divider(
-              color: Colors.black,
-              height: 5.0,
-            ),
-
-            new ListTile(
-              title: new Text('Shop Info'),
-              onTap: () {
-                Navigator.of(context).pop();
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Profile()),
-                );
-              } ,
-            ),
-
-            new ListTile(
-              title: new Text(
-                  'Feedback Info'
-              ),
-              onTap: () {
-                Navigator.of(context).pop();
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => FeedBackForm()),
-                );
-              } ,
-            ),
-
-
-
-            new ListTile(
-              title: new Text(
-                  'Credit Cards'
-              ),
-              onTap: () {
-                Navigator.of(context).pop();
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => CheckOut()),
-                );
-              } ,
-            ),
-
-
-
-          ],
-        ),
-      ),
+      drawer: CustomDrawer() 
     );
   }
 }
---
+++ restaurantsapp/lib/widgets/drawer.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:restaurantsapp/check_out.dart';
+import '../profile.dart';
+import '../feedback_form.dart';
+import '../user_info.dart';
+
+class CustomDrawer extends StatelessWidget{
+  
+  const CustomDrawer();
+
+  @override
+  Widget build(BuildContext context) {
+    return new Drawer(
+        child: ListView(
+          children: <Widget>[
+            new UserAccountsDrawerHeader(
+                accountName: new Text('Sam'),
+                accountEmail: new Text('jagateesvaran'),
+              currentAccountPicture: new CircleAvatar(
+                backgroundImage: new AssetImage('assets/images/profile_picture.jpg'),
+              ),
+            ),
+            new ListTile(
+              title: new Text('Profile Info'),
+              onTap: () {
+                Navigator.of(context).pop();
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => UserInfo()),
+                );
+              },
+            ),
+
+            new Divider(
+              color: Colors.black,
+              height: 5.0,
+            ),
+
+            new ListTile(
+              title: new Text('Shop Info'),
+              onTap: () {
+                Navigator.of(context).pop();
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Profile()),
+                );
+              } ,
+            ),
+
+            new ListTile(
+              title: new Text(
+                  'Feedback Info'
+              ),
+              onTap: () {
+                Navigator.of(context).pop();
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => FeedBackForm()),
+                );
+              } ,
+            ),
+
+
+
+            new ListTile(
+              title: new Text(
+                  'Credit Cards'
+              ),
+              onTap: () {
+                Navigator.of(context).pop();
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => CheckOut()),
+                );
+              } ,
+            ),
+
+
+
+          ],
+        ),
+      );
+  }
+}
